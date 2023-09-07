6e57ea4a396b8e32d64b3b84eb577999218516ab
Brian Lund <blund05@msn.com>
2023-09-07 15:48:09 -0600

    add basic functionality for adding tasks to to-do list

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -5,11 +5,27 @@ const App = () => {
   const [todo, setTodo] = React.useState("");
   
   // Add the handlesubmit code here
-  // function handleSubmit(e) {
-  //   e.preventDefault(); // ?
+  function handleSubmit(e) {
+    console.log('in handleSubmit()');
 
-    
-  // }
+    e.preventDefault();
+
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo.trim(), // remove extra whitespace
+      completed: false,
+    }
+
+    // todo must be a non-empty string
+    if (newTodo.text.length > 0) {
+      setTodos([...todos, newTodo])
+    } else {
+      alert('Enter a valid task')
+    }
+
+    // clear text input in form
+    setTodo("");
+  }
   
   
   // Add the deleteToDo code here
@@ -22,13 +38,20 @@ const App = () => {
 
   
 return(
-<div className ="App">
-<h1>Todo List</h1>
-<form>
-<input type ="text" align ="right" />
-<button type ="submit">Add Todo</button>
-</form>
-</div>
-);
+    <div className ="App">
+      <h1>Brian's To-do List!</h1>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type ="text" 
+          align ="right"
+          onChange={(e) => setTodo(e.target.value)}
+          placeholder="Add a new task"
+          value={todo}
+        />
+        <button type ="submit">Add Todo</button>
+      </form>
+      {todos.map((todo) => <div>{todo.text}</div>)}
+    </div>
+  );
 };
 export default App;
